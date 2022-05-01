afee7ccbd35f55ddcc8eee6235dbba59c7edf6f2
babailan <ronnelbabailan@outlook.com>
2022-05-02 00:24:20 +0800

    i wish this is the last commit >

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,9 @@ import styles from "./Footer.module.scss";
 function Footer() {
   return (
     <section data-scroll-section className={styles.container}>
-      <h1 className={styles.text}>I DUNNO WHAT TO PUT NEXT.</h1>
+      <h1 data-scroll className={styles.text}>
+        I DUNNO WHAT TO PUT NEXT.
+      </h1>
     </section>
   );
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -31,10 +31,10 @@ function Layout({ children }: Props) {
       >
         <main data-scroll-container ref={containerRef}>
           {load ? children : null}
+          {load ? <Loader /> : null}
+          {size < 696 && load ? <Warn /> : null}
         </main>
       </LocomotiveScrollProvider>
-      {load ? <Loader /> : null}
-      {size < 696 && load ? <Warn /> : null}
       {load ? <Senpai /> : null}
     </>
   );
