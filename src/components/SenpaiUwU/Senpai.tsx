7c41dfe6c47d4a6d9fc8c021b639dcb48c4e8c73
Ronnel <83863770+Babailan@users.noreply.github.com>
2022-05-02 17:22:54 +0800

    Update Senpai.tsx
    :>

--- src/components/SenpaiUwU/Senpai.tsx
+++ src/components/SenpaiUwU/Senpai.tsx
@@ -2,7 +2,7 @@ import styles from "./Senpai.module.scss";
 function Senpai() {
   return (
     <h1 className={styles.senpai}>
-      <a href="https://github.com/Babailan" target={"_blank"} rel="noreferrer">
+      <a href="https://github.com/Babailan/medusa" target={"_blank"} rel="noreferrer">
         GITHUB
       </a>
       <span className={styles.line}></span>
