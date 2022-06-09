7c517975634c576613d65278282710937db4181b
Babailan <ronelbabailan@hotmail.com>
2022-06-09 23:44:16 +0800

    change senpaiUwU

--- src/components/SenpaiUwU/Senpai.tsx
+++ src/components/SenpaiUwU/Senpai.tsx
@@ -1,12 +1,16 @@
 import styles from "./Senpai.module.scss";
 function Senpai() {
   return (
-    <h1 className={styles.senpai}>
-      <a href="https://github.com/Babailan/medusa" target={"_blank"} rel="noreferrer">
+    <h3 className={styles.senpai}>
+      <a
+        href="https://github.com/Babailan/medusa"
+        target={"_blank"}
+        rel="noreferrer"
+      >
         GITHUB
       </a>
       <span className={styles.line}></span>
-    </h1>
+    </h3>
   );
 }
 
